f21bca86f5cbe0e80986e4ea1f69e4f0318206fc
Millard Van Nixon Letrago <mvnletrago81864@liceo.edu.ph>
2026-07-10 11:09:05 +0800

    lab activity 2

---
+++ src/Book.java
@@ -0,0 +1,44 @@
+public class Book {
+
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void borrow() {
+        isBorrowed = true;
+    }
+
+    public void returnBook() {
+        isBorrowed = false;
+    }
+
+    public String describe() {
+        String status;
+
+        if (isBorrowed) {
+            status = "Borrowed";
+        } else {
+            status = "Available";
+        }
+
+        return "Title: " + title + " | Author: " + author + " | Status: " + status;
+    }
+}
