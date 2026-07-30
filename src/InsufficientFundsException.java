58f2a7fa08bf061850f4a8dce2631179f17568ef
Millard Van Nixon Letrago <mvnletrago81864@liceo.edu.ph>
2026-07-30 16:19:54 +0800

    lab-activity-5 :Mini ATM with exception handling

---
+++ src/InsufficientFundsException.java
@@ -0,0 +1,12 @@
+public class InsufficientFundsException extends Exception {
+    private final double shortfall;
+
+    public InsufficientFundsException(String message, double shortfall) {
+        super(message);
+        this.shortfall = shortfall;
+    }
+
+    public double getShortfall() {
+        return shortfall;
+    }
+}
